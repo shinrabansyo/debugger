7babf490c3abf2df4927eed2d164eca7a705a840
NakagamiYuta <guguru0014@gmail.com>
2024-11-24 23:49:05 +0900

    [add] 命令列の表示を行えるようにした (逆アセンブルはまだ)

--- src/ui/widget/inst_view.rs
+++ src/ui/widget/inst_view.rs
@@ -1,16 +1,18 @@
-use crossterm::event::KeyEvent;
+use std::cmp::max;
+
+use crossterm::event::{KeyCode, KeyEvent};
 use ratatui::buffer::Buffer;
 use ratatui::layout::Rect;
 use ratatui::widgets::{Widget, Block, Paragraph};
 use ratatui::symbols::border;
-use ratatui::style::Stylize;
-use ratatui::text::Line;
+use ratatui::style::{Color, Style, Stylize};
+use ratatui::text::{Text, Line, Span};
 
 use sb_emu::State as EmuState;
 
 pub struct InstView {
     selected: bool,
-    body: String,
+    text: Text<'static>,
 }
 
 impl Widget for InstView {
@@ -19,7 +21,7 @@ impl Widget for InstView {
                 .title(Line::from(" Instructions ".bold()).centered())
                 .border_set(if self.selected { border::THICK } else { border::ROUNDED });
 
-        Paragraph::new(self.body.as_str())
+        Paragraph::new(self.text)
             .block(block)
             .render(area, buf);
     }
@@ -27,26 +29,59 @@ impl Widget for InstView {
 
 pub struct InstViewState {
     selected: bool,
-    latest_event: String,
+    offset: i32,
 }
 
 impl InstViewState {
     pub fn new(selected: bool) -> Self {
         InstViewState {
             selected,
-            latest_event: String::new(),
+            offset: 0,
         }
     }
 
-    pub fn gen_widget(&self, _emu: &EmuState) -> InstView {
+    pub fn gen_widget(&self, emu: &EmuState) -> InstView {
+        let mut lines = vec![];
+        for row in 0..24 {
+            let mut line = vec![];
+
+            // 命令アドレス
+            let addr = (self.offset + row * 6) as usize;
+            line.push(Span::styled(
+                format!("0x{:08x}: ", addr),
+                Style::new().fg(Color::Yellow),
+            ));
+
+            // 命令
+            let raw_inst = emu.imem.read::<6>(addr).unwrap();
+            let assembly = "(TODO)";
+            if addr == emu.pc as usize {
+                line.push(Span::styled(
+                    format!("{:32} 0x{:08b}", assembly, raw_inst),
+                    Style::new().fg(Color::Red).underlined().bold(),
+                ));
+            } else {
+                line.push(Span::styled(
+                    format!("{:32} 0x{:08b}", assembly, raw_inst),
+                    Style::new().fg(Color::White),
+                ));
+            }
+
+            lines.push(Line::from(line));
+        }
+
         InstView {
             selected: self.selected,
-            body: self.latest_event.clone(),
+            text: Text::from(lines),
         }
     }
 
     pub fn handle_key_event(&mut self, event: KeyEvent) {
-        self.latest_event = format!("{:?}", event);
+        self.offset = match event.code {
+            KeyCode::Up => max(0, self.offset - 6),
+            KeyCode::Down => self.offset + 6,
+            _ => self.offset,
+        };
     }
 
     pub fn set_selected(&mut self, selected: bool) {
