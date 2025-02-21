337992ff965aa9c8517db22506adc6f2098aa45d
NakagamiYuta <guguru0014@gmail.com>
2025-02-21 20:52:57 +0900

    [fix] tui::run 関数の引数に pc, dmem, imem を追加

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,12 @@
-mod opts;
-mod load;
 mod ui;
 
 use sb_emu::State as EmuState;
 
-use opts::{parse_args, FileFormat};
-use load::{load_assembly, load_bytechar, load_bytechar2};
 use ui::UI;
 
-pub fn run() -> anyhow::Result<()> {
-    let options = parse_args();
+pub fn run(pc: u32, dmem: &[u8], imem: &[u8]) -> anyhow::Result<()> {
+    let init_state = EmuState::new(pc, &dmem, &imem);
 
-    // エミュレータ初期化
-    let (dmem, imem) = match (options.format, options.data) {
-        (FileFormat::Assembly, _) => load_assembly(&options.inst)?,
-        (FileFormat::ByteChar, Some(data)) => load_bytechar2(&data, &options.inst)?,
-        (FileFormat::ByteChar, None) => (load_bytechar(&options.inst)?, vec![]),
-    };
-    let init_state = EmuState::new(options.pc, &dmem, &imem);
-
-    // TUI 起動
     let mut terminal = ratatui::init();
     UI::new(init_state).run(&mut terminal)?;
     ratatui::restore();
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+mod opts;
+mod load;
+
+use opts::{parse_args, FileFormat};
+use load::{load_assembly, load_bytechar, load_bytechar2};
+
 fn main() -> anyhow::Result<()> {
-    sb_dbg_tui::run()
+    let options = parse_args();
+    let (dmem, imem) = match (options.format, options.data) {
+        (FileFormat::Assembly, _) => load_assembly(&options.inst)?,
+        (FileFormat::ByteChar, Some(data)) => load_bytechar2(&data, &options.inst)?,
+        (FileFormat::ByteChar, None) => (load_bytechar(&options.inst)?, vec![]),
+    };
+
+    sb_dbg_tui::run(options.pc, &dmem, &imem)
 }
