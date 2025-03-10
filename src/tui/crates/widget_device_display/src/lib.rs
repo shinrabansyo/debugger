c74d635ee2420169f89a13cf4e649ddb9a704ef8
NakagamiYuta <guguru0014@gmail.com>
2025-03-11 02:53:50 +0900

    [wip] widget_device_display

--- src/tui/crates/widget_device_display/src/lib.rs
+++ src/tui/crates/widget_device_display/src/lib.rs
@@ -1,5 +1,8 @@
+// use image::ImageReader;
 use ratatui::layout::Rect;
 use ratatui::text::Text;
+// use ratatui_image::picker::Picker;
+// use ratatui_image::{Image, Resize};
 
 use sb_emu::State as EmuState;
 use sb_dbg_tui_engine::widget::{Widget, WidgetView};
@@ -9,6 +12,12 @@ pub struct Display;
 
 impl Widget for Display {
     fn draw(&self, _: &Rect, _: &EmuState) -> WidgetView {
-        WidgetView::default().title(" Device: Display ").body(Text::raw(""))
+        // let picker = Picker::from_query_stdio().unwrap();
+        // let image = ImageReader::open("").unwrap().decode().unwrap();
+        // let mut image = Box::new(picker.new_protocol(image, Rect::new(0, 0, 100, 100), Resize::Crop(None)).unwrap());
+        // let image = Image::new(&mut image);
+
+        // WidgetView::default().title(" Device: Display ").body(image)
+        WidgetView::default().title(" Device: Display ").body(Text::raw("hello, world!"))
     }
 }
