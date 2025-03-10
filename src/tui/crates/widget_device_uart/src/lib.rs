dfe6a4bcbad1ce351545e1a401acdac123bb8a60
NakagamiYuta <guguru0014@gmail.com>
2025-03-11 02:22:58 +0900

    [update] widget_device を削除して widget_device_{gpout, uart} に分割

--- src/tui/crates/widget_device_uart/src/lib.rs
+++ src/tui/crates/widget_device_uart/src/lib.rs
@@ -2,24 +2,20 @@ use std::cmp::max;
 
 use ratatui::layout::Rect;
 use ratatui::text::Text;
-use crossterm::event::KeyEvent;
 
 use sb_emu::State as EmuState;
-
-use sb_dbg_tui_engine::widget::WidgetView;
+use sb_dbg_tui_engine::widget::{Widget, WidgetView};
 
 #[derive(Default)]
 pub struct Uart;
 
-impl Uart {
-    pub fn draw(&self, area: &Rect, emu: &EmuState) -> WidgetView {
+impl Widget for Uart {
+    fn draw(&self, area: &Rect, emu: &EmuState) -> WidgetView {
         let uart_lines = emu.devices.get_stat(0).unwrap();
         let uart_lines = uart_lines.lines().collect::<Vec<_>>();
         let view_begin_idx = max(0, (uart_lines.len() as i32) - (area.height as i32) + 2);
         let uart_output = uart_lines[view_begin_idx as usize..].join("\n");
 
-        WidgetView::default().title(" Device 0: UART ").body(Text::raw(uart_output))
+        WidgetView::default().title(" Device: UART ").body(Text::raw(uart_output))
     }
-
-    pub fn handle_key_event(&mut self, _: KeyEvent) {}
 }
--- src/tui/src/lib.rs
+++ src/tui/src/lib.rs
@@ -5,21 +5,30 @@ use sb_emu::State as EmuState;
 use sb_dbg_tui_engine::workspace::WorkspaceBuilder;
 use sb_dbg_tui_engine::UI;
 use sb_dbg_tui_widget_inst::Inst;
-use sb_dbg_tui_widget_device::Device;
+use sb_dbg_tui_widget_device_gpout::Gpout;
+use sb_dbg_tui_widget_device_uart::Uart;
 use sb_dbg_tui_widget_reg::Register;
 use sb_dbg_tui_widget_mem::Mem;
 
 pub fn run(pc: u32, dmem: &[u8], imem: &[u8]) -> anyhow::Result<()> {
     let emu = EmuState::new(pc, &dmem, &imem);
 
-    let workspace_0 = WorkspaceBuilder::default()
-        .name("Workspace 0")
-        .widget((0, 0), Box::new(Inst::default()))
-        .widget((0, 1), Box::new(Device::default()))
-        .widget((1, 0), Box::new(Register::default()))
-        .widget((1, 1), Box::new(Mem::default()))
-        .build();
-    let workspaces = [workspace_0];
+    let workspaces = [
+        WorkspaceBuilder::default()
+            .name("Workspace 0")
+            .widget((0, 0), Box::new(Inst::default()))
+            .widget((0, 1), Box::new(Uart::default()))
+            .widget((1, 0), Box::new(Register::default()))
+            .widget((1, 1), Box::new(Mem::default()))
+            .build(),
+        WorkspaceBuilder::default()
+            .name("Workspace 1")
+            .widget((0, 0), Box::new(Inst::default()))
+            .widget((0, 1), Box::new(Gpout::default()))
+            .widget((1, 0), Box::new(Register::default()))
+            .widget((1, 1), Box::new(Mem::default()))
+            .build(),
+    ];
 
     UI::new(emu, workspaces).run(&mut ratatui::init())?;
     ratatui::restore();
