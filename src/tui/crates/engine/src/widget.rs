0c93607fa0550affc0fc4b90707454428588b1d5
NakagamiYuta <guguru0014@gmail.com>
2025-03-11 02:52:00 +0900

    [update] WidgetView の定義にライフタイムを追加 (constructor が持つクロージャ用)

--- src/tui/crates/engine/src/widget.rs
+++ src/tui/crates/engine/src/widget.rs
@@ -17,31 +17,31 @@ pub trait Widget {
 }
 
 #[derive(Default)]
-pub struct WidgetView {
+pub struct WidgetView<'a> {
     selected: bool,
     title: Option<String>,
-    constructor: Option<Box<dyn FnOnce(Rect, &mut Buffer, bool, Option<String>) -> ()>>,
+    constructor: Option<Box<dyn FnOnce(Rect, &mut Buffer, bool, Option<String>) -> () + 'a>>,
 }
 
-impl ratatui::widgets::Widget for WidgetView {
+impl<'a> ratatui::widgets::Widget for WidgetView<'a> {
     fn render(self, area: Rect, buf: &mut Buffer) {
         let constructor = self.constructor.unwrap();
         constructor(area, buf, self.selected, self.title);
     }
 }
 
-impl WidgetView {
-    pub fn selected(mut self, selected: bool) -> WidgetView {
+impl<'a> WidgetView<'a> {
+    pub fn selected(mut self, selected: bool) -> WidgetView<'a> {
         self.selected = selected;
         self
     }
 
-    pub fn title(mut self, title: impl Into<String>) -> WidgetView {
+    pub fn title(mut self, title: impl Into<String>) -> WidgetView<'a> {
         self.title = Some(title.into());
         self
     }
 
-    pub fn body(mut self, body: impl ratatui::widgets::Widget + 'static) -> WidgetView {
+    pub fn body(mut self, body: impl ratatui::widgets::Widget + 'a) -> WidgetView<'a> {
         fn make_outer(area: Rect, buf: &mut Buffer, selected: bool, title: Option<String>) -> Rect {
             if let Some(title) = title {
                 let title = Line::from(title).bold().centered();
