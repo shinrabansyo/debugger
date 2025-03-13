c10cb2982b5ba1fc5831abb03a92cd9a5fb4e647
NakagamiYuta <guguru0014@gmail.com>
2025-03-13 23:17:39 +0900

    [add] コマンドラインを表示できるようにした

--- src/tui/crates/engine/src/lib.rs
+++ src/tui/crates/engine/src/lib.rs
@@ -8,19 +8,28 @@ use std::time::Duration;
 
 use crossterm::event::{KeyEvent, KeyCode};
 use crossterm::event;
+use ratatui::widgets::Clear;
+use ratatui::text::Text;
 use ratatui::{DefaultTerminal, Frame};
 
 use sb_emu::Emulator;
 
+use widget::WidgetView;
 use workspace::Workspace;
 
 pub struct UI {
+    running: bool,
+
     // ワークスペース
     workspace_id: usize,
     workspaces: Vec<Workspace>,
 
+    // コマンドライン
+    command_mode: bool,
+    command: String,
+    history: Vec<String>,
+
     // エミュレータの状態
-    running: bool,
     emu: Emulator,
     remain_exec_cnt: u32,
 }
@@ -28,9 +37,12 @@ pub struct UI {
 impl UI {
     pub fn start<const N: usize>(emu: Emulator, workspaces: [Workspace; N]) -> anyhow::Result<()> {
         let mut ui = UI {
+            running: true,
             workspace_id: 0,
             workspaces: workspaces.into_iter().collect::<Vec<_>>(),
-            running: true,
+            command_mode: false,
+            command: String::new(),
+            history: vec!["Welcome!".to_string()],
             emu,
             remain_exec_cnt: 0,
         };
@@ -64,12 +76,36 @@ impl UI {
     fn draw(&mut self, terminal: &mut DefaultTerminal) -> anyhow::Result<()> {
         terminal.draw(|frame| {
             self.draw_workspace(frame);
+            if self.command_mode {
+                self.draw_command_line(frame);
+            }
         })?;
         Ok(())
     }
 
     fn draw_workspace(&mut self, frame: &mut Frame) {
         self.workspaces[self.workspace_id].draw(frame, &self.emu);
+    }
+
+    fn draw_command_line(&mut self, frame: &mut Frame) {
+        // 画面中央に 1/4 の大きさで表示
+        let mut area = frame.area();
+        area.x = area.width / 2 - (area.width / 4);
+        area.y = area.height / 2 - (area.height / 4);
+        area.width = area.width / 2;
+        area.height = area.height / 2;
+
+        // 描画リセット
+        frame.render_widget(Clear::default(), area);
+
+        // コマンドライン描画
+        let history = self.history.join("\n") + "\n";
+        let history = history + format!("> {}", self.command).as_str();
+        let command_line = WidgetView::default()
+            .title(" Command Line ")
+            .body(Text::from(history))
+            .selected(true);
+        frame.render_widget(command_line, area);
     }
 }
 
@@ -86,6 +122,14 @@ impl UI {
     }
 
     fn handle_key_event(&mut self, event: KeyEvent) {
+        if self.command_mode {
+            self.handle_key_event_on_command(event);
+        } else {
+            self.handle_key_event_on_normal(event);
+        }
+    }
+
+    fn handle_key_event_on_normal(&mut self, event: KeyEvent) {
         match event.code {
             // エミュレータ制御
             KeyCode::Enter => self.remain_exec_cnt = 1,
@@ -94,6 +138,9 @@ impl UI {
             } else {
                 self.remain_exec_cnt = 0;
             },
+
+            // コマンドモード制御
+            KeyCode::Char('c') => self.command_mode = true,
 
             // ワークスペース切り替え
             KeyCode::Char(c) if c.is_digit(10) => {
@@ -107,6 +154,21 @@ impl UI {
 
             // 各ウィジェットでの処理
             _ => self.workspaces[self.workspace_id].on_key_pressed(event),
+        }
+    }
+
+    fn handle_key_event_on_command(&mut self, event: KeyEvent) {
+        match event.code {
+            // コマンドモード離脱
+            KeyCode::Esc => self.command_mode = false,
+
+            // コマンド入力
+            KeyCode::Char(' ') => self.command.push(' '),
+            KeyCode::Char(c) if c.is_alphanumeric() => self.command.push(c),
+            KeyCode::Backspace if !self.command.is_empty() => { self.command.pop(); },
+            KeyCode::Enter => self.command.clear(),
+
+            _ => {}
         }
     }
 }
